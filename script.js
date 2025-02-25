dd916bbdab7ecbcd93063d3d111599c19ffa5014
vitorazevedop7 <vitorazevedo.p7@gmail.com>
2025-02-25 13:36:10 -0300

    Teste 3

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('yes').addEventListener('click', function() {
-    document.getElementById('response').innerHTML = 'CONFIRMADO! &#128077;'; // Joinha emoji
+    document.getElementById('response').innerHTML = 'CONFIRMADO NIGGA! &#128077;'; // Joinha emoji
 });
 
 document.getElementById('no').addEventListener('click', function() {
