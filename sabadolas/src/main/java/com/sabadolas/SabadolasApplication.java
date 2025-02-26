4a245304ee6ed6b8af34a781e224c4459752c387
vitorazevedop7 <vitorazevedo.p7@gmail.com>
2025-02-26 10:19:07 -0300

    Primeiro commit do Projeto

---
+++ sabadolas/src/main/java/com/sabadolas/SabadolasApplication.java
@@ -0,0 +1,11 @@
+package com.sabadolas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabadolasApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SabadolasApplication.class, args);
+    }
+}
